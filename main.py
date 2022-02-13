b7ceab9fe377944d1a57703be7b4892d9ec9425c
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-13 06:27:33 +0000

    Now tells us that a guess is wrong

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ while not end_of_game:
       #Check if user is wrong.
       if guess not in chosen_word:
           #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+          print(f"{guess} is not in the word.")
           lives -= 1
           if lives == 0:
               end_of_game = True
