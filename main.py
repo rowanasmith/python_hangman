7015ae943e93c624682746661b8068a9b7c40a91
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-13 06:26:09 +0000

    Program checks for repeat guesses and alerts player, allows them to guess again

--- main.py
+++ main.py
@@ -22,34 +22,38 @@ print(f'Pssst, the solution is {chosen_word}.')
 display = []
 for _ in range(word_length):
     display += "_"
-
+guessed_letters = ""
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in guessed_letters:
+      print(f"You have already guessed {guess}, guess again!")
+    else:
+      guessed_letters += guess
 
-    #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter
+      #Check guessed letter
+      for position in range(word_length):
+          letter = chosen_word[position]
+          print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+          if letter == guess:
+              display[position] = letter
 
-    #Check if user is wrong.
-    if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You lose.")
+      #Check if user is wrong.
+      if guess not in chosen_word:
+          #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+          lives -= 1
+          if lives == 0:
+              end_of_game = True
+              print("You lose.")
 
-    #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+      #Join all the elements in the list and turn it into a String.
+      print(f"{' '.join(display)}")
 
-    #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
+      #Check if user has got all letters.
+      if "_" not in display:
+          end_of_game = True
+          print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
-    print(stages[lives])
+      #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+      print(hangman_art.stages[lives])
