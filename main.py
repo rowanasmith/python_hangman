25c92c02eaf163f2aee4a35f7182398fe31e2d0a
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-13 06:17:14 +0000

    Imported other files and used as modules

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #Step 5
 
 import random
+import hangman_art
+import hangman_words
 
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+print(hangman_art.logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
